c19288ffc6ac7d4a27d2551026000e4bc8412583
Siwin Lo <siwin.lo@gmail.com>
2019-09-29 14:29:51 -0400

    Updates

---
+++ precache-manifest.97aa07a8b907343abeb010c9b9f2ca23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b55c91843530d88bb13597b80000881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0aa7e5f3c2b0e424b0e",
+    "url": "/static/css/main.5e262071.chunk.css"
+  },
+  {
+    "revision": "97d4512a262f26812fde",
+    "url": "/static/js/2.1b0787da.chunk.js"
+  },
+  {
+    "revision": "c0aa7e5f3c2b0e424b0e",
+    "url": "/static/js/main.2ed36046.chunk.js"
+  },
+  {
+    "revision": "356b753097a40fd9fd71",
+    "url": "/static/js/runtime-main.9a8fc27b.js"
+  }
+]);
