15fe436ddb96710e77051773e64ac282777cd3e7
Siwin Lo <siwin.lo@gmail.com>
2019-09-29 18:09:05 -0400

    Updates

---
+++ precache-manifest.e40fec92776b58bd8551d80b8949df34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ef119da9f11bd760312b1ac5c46d46",
+    "url": "/Artopod/index.html"
+  },
+  {
+    "revision": "c677dd92fc06483624b2",
+    "url": "/Artopod/static/css/main.15fea1fa.chunk.css"
+  },
+  {
+    "revision": "ef035e2183396423e59a",
+    "url": "/Artopod/static/js/2.3f9a5ba8.chunk.js"
+  },
+  {
+    "revision": "c677dd92fc06483624b2",
+    "url": "/Artopod/static/js/main.e6f52172.chunk.js"
+  },
+  {
+    "revision": "96fb931355cb2a0b5cc9",
+    "url": "/Artopod/static/js/runtime-main.1b824805.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Artopod/precache-manifest.a898f2e4509ee5073abad1108be63bd7.js"
+  "/Artopod/precache-manifest.e40fec92776b58bd8551d80b8949df34.js"
 );
 
 self.addEventListener('message', (event) => {
