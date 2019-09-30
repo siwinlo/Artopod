05742a8fc56545e0f3e99321094a4dd27e5e1f32
Siwin Lo <siwin.lo@gmail.com>
2019-09-29 20:27:02 -0400

    Updates

--- precache-manifest.fad2e23400bb6e5b8cf221ee569492cf.js
+++ precache-manifest.fad2e23400bb6e5b8cf221ee569492cf.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7cae85f9246a087d3147aaf6f990597a",
+    "revision": "23639c7796a388c584da63a5d08e27f2",
     "url": "/Artopod/index.html"
   },
   {
-    "revision": "97a5893d4149835e5a12",
-    "url": "/Artopod/static/css/main.bdd64835.chunk.css"
+    "revision": "00820f58a62682c55979",
+    "url": "/Artopod/static/css/main.649ec811.chunk.css"
   },
   {
     "revision": "ef035e2183396423e59a",
     "url": "/Artopod/static/js/2.3f9a5ba8.chunk.js"
   },
   {
-    "revision": "97a5893d4149835e5a12",
+    "revision": "00820f58a62682c55979",
     "url": "/Artopod/static/js/main.f262e8de.chunk.js"
   },
   {
